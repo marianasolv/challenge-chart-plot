6b2cf71ace1f84bcbb1a6dfe7db5ec3f2152b18e
Mariana Oliveira <mariana.oliveira@uniriotec.br>
2018-10-23 22:27:12 -0300

    Initialize and Update boundaries Chart
    Added the chart library Highcharts and the functions to initiliaze the chart with empty data and to update it's boundaries created.

--- js/script.js
+++ js/script.js
@@ -17,8 +17,70 @@ function initializeCodeEditor() {
 };
 
 
+var chart = null; //Global variable to store the chart
+
+//This function will initialize a line chart with empy data
+function initializeChart() {
+  Highcharts.setOptions({
+    global: {
+      useUTC: false
+    }
+  });
+
+  var $container = $('#line-chart');
+
+  var chartingOptions = {
+    chart: {
+      renderTo: $container[0],
+      type: 'line'
+    },
+    title: {
+      text: ''
+    },
+    xAxis: {
+      type: 'datetime',
+      labels: {
+        format: "{value:%H:%M}"
+      }
+    },
+    yAxis: {
+      title: {
+        text: ''
+      }
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      itemStyle: {
+        fontFamily: 'Source Sans Pro'
+      }
+    },
+    series: [],
+    responsive: {
+      rules: [{
+        condition: {
+          minWidth: 200
+        },
+        credits: {
+          enabled: false
+        }
+      }]
+    }
+  }
+
+  chart = new Highcharts.Chart(chartingOptions);
+};
 
 //When DOM is ready, call the function to initialize the code editor
 $( document ).ready(function() {
   initializeCodeEditor();
+  initializeChart();
 });
+
+
+//This function will update de extreme values of the Chart on the x axis
+//min and max values.
+function updateChartBoundaries(min, max) {
+  chart.xAxis[0].setExtremes(min,max);
+};
